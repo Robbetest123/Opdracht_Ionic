e7b3b6b5201b36eff22f779628291ba366a3bd8e
Robbetest123 <robbe.croes@student.vives.be>
2020-11-24 19:40:41 +0100

    commit 2

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { connect as mqttConnect } from "mqtt"
 
 const Tab1: React.FC = () => {
   function test1() {
-      console.log("test pin 10");
-    }
+    console.log("test pin 10");
+  }
 
-    function test2() {
-      console.log("test pin 11");
-    }
-    return (
-    
+  function test2() {
+    console.log("test pin 11");
+
+  }
+
+  //connect to our broker
+  //IMPORTANT: you have to add "ws://" and the websockets port
+  // use "ws://test.mosquitto.org:8080" for public broker
+  let mqttClient = mqttConnect("mqtt://test.mosquitto.org:8080/ws")
+  //export MQTT
+  function useMQTTMessages() {
+    mqttClient.on("connect", function () {
+      console.log("bjhdbkg");
+      // subscribe on a topic after connected
+      mqttClient.subscribe("robbe")
+      // send a message to test if the connection is working
+      mqttClient.publish("robbe", "Connected")
+    })
+    mqttClient.on("message", function (topic, message) {
+      // show received message
+      console.log(topic, message.toString())
+    })
+  }
+
+
+  return (
+
     <IonPage>
       <IonHeader>
         <IonToolbar>
