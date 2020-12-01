e5a84aa16739a3800d3afd1960cc61d23a736fb4
Robbetest123 <robbe.croes@student.vives.be>
2020-12-01 10:35:27 +0100

    commit 7

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonToggle } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonToggle, IonItem, IonLabel  } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 import { connect as mqttConnect } from "mqtt"
@@ -23,24 +23,24 @@ const Tab2: React.FC = () => {
     if(message.toString() == "pin 33 is laag"){
       // achtergrond van de button veranderen.
       setChecked(false)
-      console.log("checked low");
+      console.log("checked 33 low");
     }
     else if(message.toString() == "Pin 33 is hoog"){
       // achtergrond van de button veranderen.
       setChecked(true)
-      console.log("checked high");
+      console.log("checked 33 high");
     }
 
 
     if(message.toString() == "pin 31 is laag"){
       // achtergrond van de button veranderen.
       setChecked2(false)
-      console.log("checked P2 low");
+      console.log("checked 31 low");
     }
     else if(message.toString() == "Pin 31 is hoog"){
       // achtergrond van de button veranderen.
       setChecked2(true)
-      console.log("checked P2 high");
+      console.log("checked 31 high");
     }
   })
   return (
@@ -51,8 +51,14 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-      <IonToggle checked={checked} disabled={true} onIonChange={e => setChecked(e.detail.checked)} />
-      <IonToggle checked={checked2} disabled={true} onIonChange={e => setChecked2(e.detail.checked)} />
+      <IonItem>
+            <IonLabel>Pin 33</IonLabel>
+            <IonToggle checked={checked} disabled={true} onIonChange={e => setChecked(e.detail.checked)} />
+          </IonItem>
+          <IonItem>
+            <IonLabel>Pin 31</IonLabel>
+            <IonToggle checked={checked2} disabled={true} onIonChange={e => setChecked2(e.detail.checked)} />
+          </IonItem>
       </IonContent>
     </IonPage>
   );
